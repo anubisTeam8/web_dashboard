a1279bbf65f49db777cec43d4b73e595b9ea3724
Ahmed Mahdy <mo.a9tay@gmail.com>
2022-08-25 23:08:19 +0200

    Added API

--- lib/views/patientsPage.dart
+++ lib/views/patientsPage.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:web_dashboard/colors.dart';
-import 'package:web_dashboard/widgets/calendarWidget.dart';
-import 'package:web_dashboard/widgets/doctorsWidget.dart';
-import 'package:web_dashboard/widgets/pieWidget.dart';
+import 'package:web_dashboard/services/dashboard_service.dart';
+import 'package:web_dashboard/widgets/status_card.dart';
 import 'package:web_dashboard/widgets/tableWidget.dart';
 
 class PatientsPage extends StatelessWidget {
@@ -12,55 +12,103 @@ class PatientsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 50.w, vertical: 30.h),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(width: 1570.w,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(" Medical Idea", style: TextStyle(fontSize: 40.sp, color: blue, fontWeight: FontWeight.bold),),
-                    Row(
-                      children: [
-                        SizedBox(height: 80.h, width: 450.w, child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "Search",
-                            contentPadding: EdgeInsets.symmetric(horizontal: 30.w),
-                            fillColor: Colors.white, filled: true,
-                            suffixIcon: const Icon(Icons.search),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(50.sp),
-                            )
-                          ),
-                        )),
-                        SizedBox(width: 100.w,),
-                        const CircleAvatar(),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 15.h,),
-              Row(
-                children: [
-                  const TableWidget(),
-                  Column(
-                    children: const [
-                      CalenderWidget(),
-                      DoctorsWidget(),
-                      PieWidget(),
-                    ],
+    return GetBuilder<DashboardService>(
+        init: DashboardService(),
+        builder: (controller) {
+          return Scaffold(
+            body: controller.isLoading.value
+                ? const Center(
+                    child: CircularProgressIndicator(),
                   )
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+                : controller.dashboardModel == null
+                    ? const Center(
+                        child: Text('You don\'t have acces to this data.'),
+                      )
+                    : Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 50.w, vertical: 30.h),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                width: 1570.w,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      " Medical Idea",
+                                      style: TextStyle(
+                                          fontSize: 40.sp,
+                                          color: blue,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Row(
+                                      children: [
+                                        SizedBox(
+                                            height: 80.h,
+                                            width: 450.w,
+                                            child: TextField(
+                                              decoration: InputDecoration(
+                                                  hintText: "Search",
+                                                  contentPadding:
+                                                      EdgeInsets.symmetric(
+                                                          horizontal: 30.w),
+                                                  fillColor: Colors.white,
+                                                  filled: true,
+                                                  suffixIcon:
+                                                      const Icon(Icons.search),
+                                                  border: OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            50.sp),
+                                                  )),
+                                            )),
+                                        SizedBox(
+                                          width: 100.w,
+                                        ),
+                                        const CircleAvatar(),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 15.h,
+                              ),
+                              Row(
+                                children: [
+                                  TableWidget(
+                                    patientModel:
+                                        controller.dashboardModel!.patients!,
+                                  ),
+                                  Column(
+                                    children: [
+                                      StatusCard(
+                                        title: 'All Accepted',
+                                        statNum: controller
+                                            .dashboardModel!.allAccepted!,
+                                      ),
+                                      StatusCard(
+                                        title: 'All Rejected',
+                                        statNum: controller
+                                            .dashboardModel!.allRejected!,
+                                      ),
+                                      StatusCard(
+                                        title: 'All No Answer',
+                                        statNum: controller
+                                            .dashboardModel!.allNoAnswer!,
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+          );
+        });
   }
 }
--- lib/widgets/tableWidget.dart
+++ lib/widgets/tableWidget.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:web_dashboard/colors.dart';
+import 'package:web_dashboard/models/patient_model.dart';
 
 class TableWidget extends StatelessWidget {
-  const TableWidget({Key? key}) : super(key: key);
-
+  const TableWidget({Key? key, required this.patientModel}) : super(key: key);
+  final List<PatientModel> patientModel;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -13,63 +14,77 @@ class TableWidget extends StatelessWidget {
       child: Card(
         child: Column(
           children: [
-            SizedBox(height: 30.h,),
+            SizedBox(
+              height: 30.h,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Latest Patient Data", style: TextStyle(fontSize: 30.sp, color: grey, fontWeight: FontWeight.bold),),
-                SizedBox(width: 700.w,),
+                Text(
+                  "Latest Patient Data",
+                  style: TextStyle(
+                      fontSize: 30.sp,
+                      color: grey,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  width: 700.w,
+                ),
                 Card(
                   child: Row(
                     children: [
-                      Icon(Icons.filter_list_outlined, color: grey, size: 45.sp,),
-                      Text(" Filters ", style: TextStyle(color: grey, fontSize: 30.sp),)
+                      Icon(
+                        Icons.filter_list_outlined,
+                        color: grey,
+                        size: 45.sp,
+                      ),
+                      Text(
+                        " Filters ",
+                        style: TextStyle(color: grey, fontSize: 30.sp),
+                      )
                     ],
                   ),
                 )
               ],
             ),
-            SizedBox(height: 1065.h, width: 1060.w,
+            SizedBox(
+              height: 1065.h,
+              width: 1060.w,
               child: SingleChildScrollView(
                 scrollDirection: Axis.vertical,
                 child: SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
                   child: DataTable(
-                    headingTextStyle: TextStyle(color: grey, fontWeight: FontWeight.bold, fontSize: 30.sp),
-                    dataTextStyle: TextStyle(color: grey, fontWeight: FontWeight.bold, fontSize: 30.sp),
+                    headingTextStyle: TextStyle(
+                        color: grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.sp),
+                    dataTextStyle: TextStyle(
+                        color: grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.sp),
                     border: TableBorder.symmetric(inside: BorderSide.none),
                     columns: const [
-                      DataColumn(label: Text("NO.")),
-                      DataColumn(label: Text("Date in")),
+                      DataColumn(label: Text("ID")),
+                      DataColumn(label: Text("Email")),
                       DataColumn(label: Text("Name")),
-                      DataColumn(label: Text("Age")),
-                      DataColumn(label: Text("Gender")),
-                      DataColumn(label: Text("Last Visit")),
-                      DataColumn(label: Text("Setting")),
+                      DataColumn(label: Text("Phone")),
+                      DataColumn(label: Text("No Answer")),
+                      DataColumn(label: Text("Accepted")),
+                      DataColumn(label: Text("Rejected")),
                     ],
-                    rows: List.generate(20, (index) => DataRow(cells: [
-                      const DataCell(Text("01")),
-                      const DataCell(Text("22/2/2222")),
-                      const DataCell(Text("Ali Mohamed")),
-                      const DataCell(Text("24")),
-                      const DataCell(Text("Male")),
-                      const DataCell(Text("33/3/3333")),
-                      DataCell(Row(children: [
-                        Container(padding: EdgeInsets.all(8.sp),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25), color: Colors.red,
-                          ),
-                          child: Icon(Icons.close, color: Colors.white, size: 30.sp,),
-                        ),
-                        SizedBox(width: 5.w,),
-                        Container(padding: EdgeInsets.all(8.sp),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25), color: Colors.green,
-                          ),
-                          child: Icon(Icons.check, color: Colors.white, size: 30.sp,),
-                        ),
-                      ],)),
-                    ])),
+                    rows: List.generate(patientModel.length, (index) {
+                      final PatientModel patient = patientModel[index];
+                      return DataRow(cells: [
+                        DataCell(SelectableText("${patient.id}")),
+                        DataCell(SelectableText('${patient.email}')),
+                        DataCell(SelectableText("${patient.name}")),
+                        DataCell(SelectableText("${patient.phone}")),
+                        DataCell(SelectableText("${patient.noAnswer}")),
+                        DataCell(SelectableText("${patient.accepted}")),
+                        DataCell(SelectableText("${patient.rejected}")),
+                      ]);
+                    }),
                   ),
                 ),
               ),
